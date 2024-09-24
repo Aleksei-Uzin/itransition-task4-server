efe5b4159916a1c0c2449646e8b9b66b875d17c9
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-09-24 10:56:26 +0400

    update: users routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,38 @@
+import admin from 'firebase-admin'
 import express from 'express'
 import { ObjectId } from 'mongodb'
 import { db } from '../db/connection.js'
+
+const databaseURL = process.env.FIREBASE_DATABASE_URL
+
+const type = process.env.TYPE
+const project_id = process.env.PROJECT_ID
+const private_key_id = process.env.PRIVATE_KEY_ID
+const private_key = process.env.PRIVATE_KEY
+const client_email = process.env.CLIENT_EMAIL
+const client_id = process.env.CLIENT_ID
+const auth_uri = process.env.AUTH_URI
+const token_uri = process.env.TOCKEN_URI
+const auth_provider_x509_cert_url = process.env.AUTH_PROVIDER
+const client_x509_cert_url = process.env.CLIENT_X509
+const universe_domain = process.env.UNIVERSE_DOMAIN
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type,
+    project_id,
+    private_key_id,
+    private_key,
+    client_email,
+    client_id,
+    auth_uri,
+    token_uri,
+    auth_provider_x509_cert_url,
+    client_x509_cert_url,
+    universe_domain,
+  }),
+  databaseURL,
+})
 
 const router = express.Router()
 
@@ -41,19 +73,20 @@ router.post('/', async (request, response) => {
 router.patch('/:id', async (request, response) => {
   try {
     const query = { _id: new ObjectId(request.params.id) }
+    const status = request.body.status
     const updates = {
       $set: {
-        name: request.body.name,
-        email: request.body.email,
-        creationTime: request.body.creationTime,
-        lastSignInTime: request.body.lastSignInTime,
-        status: request.body.status,
-        authUID: request.body.authUID,
+        status,
       },
     }
 
-    let collection = await db.collection('users')
-    let result = await collection.updateOne(query, updates)
+    const collection = await db.collection('users')
+    const result = await collection.updateOne(query, updates)
+
+    await admin.auth().updateUser(request.body.uid, {
+      disabled: !status,
+    })
+
     response.send(result).status(200)
   } catch (err) {
     console.error(err)
@@ -61,11 +94,33 @@ router.patch('/:id', async (request, response) => {
   }
 })
 
-router.delete('/:id', async (request, response) => {
+router.patch('/', async (request, response) => {
+  try {
+    const email = request.body.email
+    const updates = {
+      $set: {
+        lastSignInTime: request.body.lastSignInTime,
+      },
+    }
+
+    const collection = await db.collection('users')
+    const { _id } = await collection.findOne({ email })
+    const result = await collection.updateOne({ _id }, updates)
+    response.send(result).status(200)
+  } catch (err) {
+    console.error(err)
+    response.status(500).send('Error updating record')
+  }
+})
+
+router.delete('/:id/:uid', async (request, response) => {
   try {
     const query = { _id: new ObjectId(request.params.id) }
     const collection = await db.collection('users')
     const result = await collection.deleteOne(query)
+
+    await admin.auth().deleteUser(request.params.uid)
+
     response.send(result).status(200)
   } catch (err) {
     console.error(err)
