edca285be4743943380666b11eeb68a3d9fd8127
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-09-26 11:49:54 +0400

    update: users routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin'
 import express from 'express'
-import { ObjectId } from 'mongodb'
+import { ObjectId, MongoServerError } from 'mongodb'
 import { db } from '../db/connection.js'
 
 const databaseURL = process.env.FIREBASE_DATABASE_URL
@@ -20,6 +20,20 @@ const universe_domain = process.env.UNIVERSE_DOMAIN
 admin.initializeApp({
   credential: admin.credential.cert({
     /* credentials */
+    type: 'service_account',
+    project_id: 'itransition-task4-auth-app',
+    private_key_id: '05bbb4cd622995856206e789187c1bf5de81e7d0',
+    private_key:
+      '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDSp5TNzKjfNGkh\nFhnfbC3SeMyXR4BXxT1IjyJd0N9V5WLZXcLc0zSaD0q4RbWQ3ONzeUwbNOJnNUlD\nKSh8bWR3vIFgy08k6DCXuzLuikxPksKa3YhbY6Kms4xj3XdY9Moa3tWtHsoX2EJ4\nkchmO5AGuE+qNzbhrioqnIb8rtcNx1ygJSV5Ar4nxMI+bSKa9au/uZDxBFTDefLA\nRRQhqaXGI2sIy2nJZJMCVFQEeT1nqAZL4NVsh/NU2G1CU7PVDnutyT0N66PYQDS3\nrgpUFhWMTWnwjSXyhYqehGq1ag6xcZGzwSKpGiHogIqV0QLIK34PrDfEL6m+JT2I\nFsbEZbnFAgMBAAECggEACwoaO8DGdtAN1FyOpY1LR3TqL+Fg4filsghCQk1gbuVz\nDTYjMKRm5M35WffZQIq65sHMEb7vQH5j5P+irgajlbzMwPBWqPX8Ge+HB0Y7Io5r\n4WbLrxJlIzxUlqU0DHbjoo3b/9MVvzMnpJ7rNCRZ82g0UJTNLP2OGyaR0mstPP0F\nz4xN/S3P/dd8I8uk1Tnhd5KmLq9IL+EpTw7ae0F1vOkan+rLa0x9T/V/x16GAD0K\nNtWLRI7aL2JWOdwcNXiQUhqvaWLGNS03X6NaYQOtikaiBjUPWvpy5A/r9TUJISxI\ndrrmt+g3pA4T6q3FcS1csjooabW/W9LCn3WdQeW6IQKBgQDrZ7jeo+5pVAeG6hfd\nltmgOloZkW17z2QUnOigoE0Uyeol5Il4lQDB8z2LR7qydQvAJdS2Vk+0S4KLkY2x\nmpOBBpH8QeRQSlkg/enWs97tIdKgVlKxOXMgAU+qzyxtgpEr/KoekQwzN+axRKoT\nmZLcZxUEn5g1orEXWYQ7zz/PEQKBgQDlFYfaWAUTXxbdZBOhJ4lNkJ6UrdtIYBoa\nMwCBgqISw+KK8da7WBmuN2QGWkHjVHm6sqTEZ2tvgXOTooL+ftk13GXpnqiU9wGu\n7JWTtwUQfiUL8/cuUbPgAqDwkBzh51Oo6Pc6vpOcwKXG7J+2uTa8TJWpS5AbQMBB\nmG+H+aindQKBgQCq+CQOn2USXpMw62f16g+ZDYhOtY6cs11ukx9pOcOB67wBVWB6\nkU2Bj5YNSEVSQywLm+5Hl5w/x+/CSi5s5WVn+kuguIKHxdfB3GgvJWgtNblx9gAM\n8zqn7gvcdKdByeiB1/n+RT5lHJ6R9NrJbWp6ZOug9gBoRAb2fG9LuRQQ4QKBgQCE\nbwkFqgLtKvit7lAGxzNyGtwCeeueEKimYru1I08M6B9MUfjgf1MBS1ZQ8dbLVCe3\nTdVBEcSjRsQJIlb2R2DM6WglQaqVy6I2fOEP7ytHWJWULmrEZUlGhF6EE9wOoMvV\nhzB9znTJlvl/YYYHfVVuUBvpLhzWFH3Qrg40HplojQKBgEvdrKhHUs3dwA4M6bFw\nUkoLJtrnx6q0uO2BKbW1jpv7KO1ftc31WQbnn0YFFMjiASbbKdMQmgWuxBzdS8jo\n3YSbn95XyF8oujkfkMhWZZ/GXjHspzx36uKqvQzuUx0vPAVPSn5LHNaiEw3kilWJ\ndwLzcDBv5L2D89hmlT5g727G\n-----END PRIVATE KEY-----\n',
+    client_email:
+      'firebase-adminsdk-7qiza@itransition-task4-auth-app.iam.gserviceaccount.com',
+    client_id: '103591005007629044900',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+      'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7qiza%40itransition-task4-auth-app.iam.gserviceaccount.com',
+    universe_domain: 'googleapis.com',
   }),
   databaseURL,
 })
@@ -42,6 +56,8 @@ router.get('/:id', async (request, response) => {
 })
 
 router.post('/', async (request, response) => {
+  const keyValueField = 'email'
+
   try {
     const newDocument = {
       name: request.body.name,
@@ -49,14 +65,27 @@ router.post('/', async (request, response) => {
       creationTime: request.body.creationTime,
       lastSignInTime: request.body.lastSignInTime,
       status: request.body.status,
-      authUID: request.body.authUID,
     }
     const collection = await db.collection('users')
     const result = await collection.insertOne(newDocument)
+    collection.createIndex({ [keyValueField]: 1 }, { unique: true })
     response.send(result).status(204)
   } catch (err) {
-    console.error(err)
-    response.status(500).send('Error adding record')
+    let errMessage
+
+    if (err instanceof MongoServerError) {
+      switch (err.code) {
+        case 11000: {
+          const duplicateField = err.keyValue[keyValueField]
+          errMessage = `Duplication Error: ${duplicateField} already exists`
+          break
+        }
+        default:
+          errMessage = err.errmsg
+      }
+    }
+
+    response.status(500).send(errMessage)
   }
 })
 
@@ -70,7 +99,8 @@ router.patch('/:id', async (request, response) => {
       },
     }
 
-    await admin.auth().updateUser(request.body.uid, {
+    const { uid } = await admin.auth().getUserByEmail(request.body.email)
+    await admin.auth().updateUser(uid, {
       disabled: !status,
     })
 
@@ -103,13 +133,14 @@ router.patch('/', async (request, response) => {
   }
 })
 
-router.delete('/:id/:uid', async (request, response) => {
+router.delete('/:id/:email', async (request, response) => {
   try {
     const query = { _id: new ObjectId(request.params.id) }
     const collection = await db.collection('users')
     const result = await collection.deleteOne(query)
 
-    await admin.auth().deleteUser(request.params.uid)
+    const { uid } = await admin.auth().getUserByEmail(request.params.email)
+    await admin.auth().deleteUser(uid)
 
     response.send(result).status(200)
   } catch (err) {
